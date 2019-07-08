bd4ee7af26912fc1358fc51155a349b561a56d19
Greg Balke <gbalke@berkeley.edu>
2019-07-08 15:30:22 -0700

    Fixing backlight support

--- install-surface.sh
+++ install-surface.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
+DRIVERS=$HOME/drivers
+if ! (grep -qRi surface /etc/udev/); then
+  git clone https://github.com/jakeday/linux-surface.git $DRIVERS
+  bash $DRIVERS/setup.sh
+  rm -rf $DRIVERS
+fi
 
+KERNELS=$HOME/kernels
+mkdir -p kernels
+echo "Get the latest kernel at: https://github.com/jakeday/linux-surface/releases"
+echo "Sign the kernel following: https://github.com/jakeday/linux-surface/blob/master/SIGNING.md"
+
+BKLT_CONF=/etc/X11/xorg.conf
+if ! (grep -q intel_backlight $BKLT_CONF); then
+  echo "
+Section \"Device\"
+Identifier  \"Card0\"
+Driver      \"intel\"
+Option      \"Backlight\"  \"intel_backlight\"
+EndSection
+  " | sudo tee -a $BKLT_CONF
+fi
